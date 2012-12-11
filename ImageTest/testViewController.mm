fa7edb913bc6b37c6ae8a19e69ac4926c3e973a5
Jessica Todd <jetodd@oucs1158.otago.ac.nz>
2012-12-12 11:17:59 +1300

    Updated now that I can unprotect points

--- ImageTest/testViewController.mm
+++ ImageTest/testViewController.mm
@@ -20,12 +20,11 @@ using namespace Eigen;
 @synthesize mat, image, mat1, image1;
 @synthesize imageView = _imageView;
 
-std::vector <Eigen::Vector4d> points3D;
+std::vector <Eigen::Vector4d> points4D;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"Hi");
 	// Do any additional setup after loading the view, typically from a nib.
     image = [UIImage imageNamed:@"/IMG_0055.JPG"];
     image1 = [UIImage imageNamed:@"/IMG_0056.JPG"];
@@ -33,154 +32,159 @@ std::vector <Eigen::Vector4d> points3D;
     mat1 = [self cvMatFromUIImage:image1];
     cv::resize(mat, mat, cvSize(352, 288));
     cv::resize(mat1, mat1, cvSize(352, 288));
-        cv::cvtColor(mat, mat, CV_BGR2GRAY);
-        cv::cvtColor(mat1, mat1, CV_BGR2GRAY);
+    cv::cvtColor(mat, mat, CV_BGR2GRAY);
+    cv::cvtColor(mat1, mat1, CV_BGR2GRAY);
+    
+    Matrix3d K(Matrix3d::Identity());
+    
+    K(0,0) = 432.02360;
+    K(0,1) = 0.0;
+    K(0,2) = 177.67501;
+    K(1,1) = 473.77855;
+    K(1,2) = 134.13891;
     
     // All the variables! ALL OF THEM!
-    cv::Mat descriptors, h1, h2, img_keypoints, img_matches, fun, camProOne, camProTwo, points, points2, n;
-    std::vector<cv::DMatch> matches, matches2, better;
-    std::vector<cv::KeyPoint> keypoints, keypoints2;
-    std::vector<cv::Point2f> k, k1;
     std::vector<uchar> inliers;
-
+    cv::Mat descriptors, img_matches, fun, tri, inliers1, inliers2;
+    std::vector<cv::DMatch> matches, matches2, better, final;
+    std::vector<cv::KeyPoint> keypoints, keypoints2;
+    std::vector<cv::Point2f> points, points2;
+    Eigen::Matrix3d fund;
+    std::vector <Eigen::Vector4d> points3D;
+    
     cv::ORB ex(80, 1.2, 8, 31, 0, 2, 0, 10);
     ex(mat, cv::Mat(), keypoints, descriptors);
     ex(mat1, cv::Mat(), keypoints2, descriptors2);
-
+    
     cv::BFMatcher m(cv::NORM_HAMMING, true);
     
-        // Certain numbers made the application crash for no particular reason, opencv is weird
-        if ((descriptors2.rows == 0 && descriptors.rows > 0) || (descriptors.rows == 0 && descriptors2.rows > 0)) {
-            NSLog(@":K crash!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            return;
-        } else {
-            // Do the matchy thing!
-            m.match(descriptors, descriptors2, matches);
-            m.match(descriptors2, descriptors, matches2);
-        }
-        
-        // Need to filter out good matches here (k1->k2 && k2->k1)
-        for (int i = 0; i < matches.size(); i++) {
-            for (int j = 0; j < matches2.size(); j++) {
-                if (matches[i].queryIdx == matches2[j].trainIdx) {
-                    if (matches[i].trainIdx == matches2[j].queryIdx ) {
-                        better.push_back(matches[i]);
-                    }
+    // Certain numbers made the application crash for no particular reason, opencv is weird
+    if ((descriptors2.rows == 0 && descriptors.rows > 0) || (descriptors.rows == 0 && descriptors2.rows > 0)) {
+        NSLog(@":K crash!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        return;
+    } else {
+        // Do the matchy thing!
+        m.match(descriptors, descriptors2, matches);
+        m.match(descriptors2, descriptors, matches2);
+    }
+    
+    // Need to filter out good matches here (k1->k2 && k2->k1)
+    for (int i = 0; i < matches.size(); i++) {
+        for (int j = 0; j < matches2.size(); j++) {
+            if (matches[i].queryIdx == matches2[j].trainIdx) {
+                if (matches[i].trainIdx == matches2[j].queryIdx ) {
+                    better.push_back(matches[i]);
                 }
             }
         }
-        
-        // Take the good matches and push them over here
-        for (int i = 0; i < better.size(); i++) {
-            k1.push_back(keypoints2[better[i].trainIdx].pt);
-            k.push_back(keypoints[better[i].queryIdx].pt);
+    }
+    
+    // Take the good matches and push them over here
+    for (int i = 0; i < better.size(); i++) {
+        points2.push_back(keypoints2[better[i].trainIdx].pt);
+        points.push_back(keypoints[better[i].queryIdx].pt);
+    }
+    
+    fun = cv::findFundamentalMat(points, points2, inliers, cv::FM_RANSAC, 1.0, 0.9999);
+    
+    for (int i = 0; i < points.size(); i++) {
+        if (inliers[i] > 0) {
+            final.push_back(better[i]);
         }
-        
-        // Distortion and calibration matrices, pre-computed with Matlab
-        double tmp[5][1] = {0.01519, -0.05698, 0.00683, 0.01324, 0.0};
-        cv::Mat distortion = cv::Mat(5, 1, CV_64F, tmp);
-        double temp[3][3] = {{432.02360, 0.0, 177.67501}, {0.0, 473.77855, 134.13891}, {0.0, 0.0, 1.0}};
-        cv::Mat camMatrix= cv::Mat(3, 3, CV_64F, temp);
-        
-        if (k.size() > 0 && k1.size() > 0) {
-            // From here on everything is magic, even though by now we have determined that magic is not real
-            Matrix3d K(Matrix3d::Identity());
-            
-            K(0,0) = 432.02360;
-            K(0,1) = 0.0;
-            K(0,2) = 177.67501;
-            K(1,1) = 473.77855;
-            K(1,2) = 134.13891;
-            
-            fun = cv::findFundamentalMat(k, k1, inliers, cv::FM_RANSAC, 1.0, 0.9999);
-            size_t inlierCount = 0;
-            for (size_t i = 0; i < inliers.size(); ++i) {
-                if (inliers[i] > 0) {
-                    ++inlierCount;
-                }
-            }
-            
-            Eigen::Matrix3d Kinv = K.inverse();
-            
-            cv::Mat inliers1(inlierCount, 2, CV_64F);
-            cv::Mat inliers2(inlierCount, 2, CV_64F);
-            
-            size_t ix = 0;
-            Eigen::Vector3d p2d_h;
-            
-            for (int i = 0; i < inliers.size(); ++i) {
-                if (inliers[i] > 0) {
-                    p2d_h << k.at(i).x, k.at(i).y, 1;
-                    p2d_h = Kinv*p2d_h;
-                    inliers1.at<double>(ix, 0) = p2d_h(0)/p2d_h(2);
-                    inliers1.at<double>(ix, 1) = p2d_h(1)/p2d_h(2);
-                    p2d_h << k1.at(i).x, k1.at(i).y, 1;
-                    p2d_h = Kinv*p2d_h;
-                    inliers2.at<double>(ix, 0) = p2d_h(0)/p2d_h(2);
-                    inliers2.at<double>(ix, 1) = p2d_h(1)/p2d_h(2);
-                    ++ix;
-                }
-            }
-            
-            // Prints number of inliers
-            // NSLog(@"(%i, %i)  (%i, %i)", inliers1.rows, inliers1.cols, inliers2.rows, inliers2.cols);
-            
-            if (inlierCount >= 8) {
-                cv::Mat E = cv::findFundamentalMat(inliers1, inliers2, cv::FM_8POINT);
-                for (int i = 0; i < E.rows; i++) {
-                    for (int j = 0; j < E.cols; j++) {
-                        NSLog(@"Ess: %f", E.at<double>(i, j));
-                    }
-                    NSLog(@"");
-                }
-                Eigen::Matrix3d R;
-                Eigen::Vector3d t;
-                points = (cv::Mat) k;
-                points2 = (cv::Mat) k1;
-                decomposeEssentialMatrix(E, inliers1, inliers2, &R, &t);
-                
-                if (R.determinant() < 0) {
-                    NSLog(@"Found negative determinant, recomputing E");
-                    decomposeEssentialMatrix(-E, points, points2, &R, &t);
-                }
-                
-                NSLog(@"translation: %f %f %f", t.x(), t.y(), t.z());
-                
-                NSLog(@"rotation:");
-                for (int i = 0; i < R.RowsAtCompileTime; i++) {
-                    for (int j = 0; j < R.ColsAtCompileTime; j++) {
-                        NSLog(@"%f", R(i, j));
-                    }
-                }
-                
-                camProOne = getProjectionMatrix(camMatrix, Eigen::Matrix3d::Identity(), Eigen::Vector3d::Zero());
-                camProTwo = getProjectionMatrix(camMatrix, R, t);
-                
-                std::vector<Eigen::Matrix<double, 3, 4>> P(2);
-                std::vector<Eigen::Vector3d> p(2);
-                
-                P[0].block<3, 3>(0, 0) = Eigen::Matrix3d::Identity();
-                P[0].col(3) = Eigen::Vector3d::Zero();
-                P[1].block(0,0,3,3) = R;
-                P[1].col(3) = t;
-                
-                for (int i = 0; i < inliers1.rows; i++) {
-                    p[0] << inliers1.at<double>(i,0), inliers1.at<double>(i,1), 1;
-                    p[1] << inliers2.at<double>(i,0), inliers2.at<double>(i,1), 1;
-                    
-                    points3D.push_back(triangulate(P, p));
-                }
-                
-                // Prints the inliers 3D points
-                //                    for (int i = 0; i < points3D.size(); i++) {
-                //                        NSLog(@"(%f, %f, %f, %f)", points3D[i].x(), points3D[i].y(), points3D[i].z(), points3D[i].w());
-                //                    }
-            }
+    }
+    
+    points.clear();
+    points2.clear();
+    
+    for (int i = 0 ; i < final.size(); i++) {
+        points2.push_back(keypoints2[final[i].trainIdx].pt);
+        points.push_back(keypoints[final[i].queryIdx].pt);
+    }
+    
+    for (int i = 0; i < fun.cols; i++) {
+        for (int j = 0; j < fun.rows; j++) {
+            fund(i, j) = fun.at<double>(i, j);
         }
-        drawMatches(mat, keypoints, mat1, keypoints2, better, img_matches);
-        image = [self UIImageFromCVMat:img_matches];
+    }
+    
+    std::vector<Eigen::Matrix<double, 3, 4>> P(2);
+    std::vector<Eigen::Vector3d> p(2);
+    
+    P[0].block<3, 3>(0, 0) = Eigen::Matrix3d::Identity();
+    P[0].col(3) = Eigen::Vector3d::Zero();
+    P[1] = [self calcProj:fund];
+    
+    //    for (int i = 0; i < fun.cols; i++) {
+    //        for (int j = 0; j < fun.rows; j++) {
+    //            NSLog(@"Fundamental matrix: %f", fun.at<double>(i, j));
+    //        }
+    //    }
+    //
+    //    for (int i = 0; i < P[0].cols(); i++) {
+    //        for (int j = 0; j < P[0].rows(); j++) {
+    //            NSLog(@"Projection matrix: %f   %f", P[0](j, i), P[1](j, i));
+    //        }
+    //    }
+    Eigen::Matrix<double, 3, 3> f;
+    
+    for (int i = 0; i < fun.cols; i++) {
+        for  (int j = 0; j < fun.rows; j++) {
+            f(i, j) = fun.at<double>(i, j);
+        }
+    }
+    
+    Eigen::Vector3d a, b;
+    
+    for (int i = 0; i < points2.size(); i++) {
+        a << points.at(i).x, points.at(i).y, 1;
+        b << points2.at(i).x, points2.at(i).y, 1;
+        debugStuff(a, b, f);
+    }
+    
+    checkStuff(P[0], P[1], f);
+    
+    
+    //    for (int i = 0; i < P[1].rows(); i++) {
+    //        NSLog(@"%f", P[1](i, 3));
+    //    }
+    
+    //    NSLog(@"Points : %li", points.size());
+    Eigen::Vector3d p2d_h;
+    
+    // Need to pull out the points that are inliers into Vector3D's
+    // Then write the triangulate method
+    for (int i = 0; i < final.size(); i++) {
+        //      NSLog(@"1: (%f, %f) 2: (%f, %f)", points.at(i).x, points.at(i).y, points2.at(i).x, points2.at(i).y);
+        p[0] << points.at(i).x, points.at(i).y, 1;
+        p[1] << points2.at(i).x, points2.at(i).y, 1;
+        
+        points4D.push_back(triangulate(P, p));
+    }
+    
+    //    for (int i = 0; i < points4D.size(); i++) {
+    //        NSLog(@"(%f %f %f %f)", points4D[i].x(), points4D[i].y(), points4D[i].z(), points4D[i].w());
+    //    }
+    
+    for (int i = 0; i < points4D.size(); i++) {
+        points3D.push_back(convertCartesian(points4D[i]));
+    }
+    
+    std::vector<cv::Point3d> c;
+    cv::Point3d s;
+    Eigen::Vector3d result;
+    
+    for (int i = 0; i < points3D.size(); i++) {
+        result = P[0] * points3D[i];
+        cv::circle(mat, cvPoint(result.x()/result.z(), result.y()/result.z()), 4, cv::Scalar(255, 83, 96));
+        NSLog(@"Original: %f %f", points.at(i).x, points.at(i).y);
+        NSLog(@"Calculat: %f %f", result.x()/result.z(), result.y()/result.z());
+        //NSLog(@"Triangulised point: %f %f %f %f", points3D[i].x(), points3D[i].y(), points3D[i].z(), points3D[i].w());
+    }
+    
+    
+    //drawMatches(mat, keypoints, mat1, keypoints2, final, img_matches);
+    image = [self UIImageFromCVMat:mat];
     _imageView.image = image;
-
 }
 
 Eigen::Vector4d triangulate(const std::vector<Eigen::Matrix<double, 3, 4>>&
@@ -210,39 +214,107 @@ Eigen::Vector4d triangulate(const std::vector<Eigen::Matrix<double, 3, 4>>&
     return svd.matrixV().col(3);
 }
 
-cv::Mat getProjectionMatrix(cv::Mat intrinsicMatrix, Matrix3d rotationMatrix, Vector3d translationVector) {
-    cv::Mat cameraMatrix = cvCreateMat(3,4,CV_64FC1);
-    cv::Mat rotationTranslationMatrix = cvCreateMat(3,4,CV_64FC1);
+Eigen::Matrix<double, 3, 4> joinMatrixVector(Matrix3d m, Vector3d v) {
+    Eigen::Matrix<double, 3, 4> join;
     
     // Glue the rotation and translation matrices together
     for(int i=0; i<3; i++) {
         for(int j=0; j<3; j++) {
-            rotationTranslationMatrix.at<double>(i, j) = rotationMatrix(i, j);
+            join(i, j) = m(i, j);
         }
     }
     
     for(int i=0; i<3; i++) {
-        rotationTranslationMatrix.at<double>(i, 3) = translationVector(i, 0);
+        join(i, 3) = v(i, 0);
     }
     
-        for (int i = 0; i < rotationTranslationMatrix.rows; i++) {
-            for (int j = 0; j < rotationTranslationMatrix.cols; j++) {
-                NSLog(@"%f", rotationTranslationMatrix.at<double>(i, j));
-            }
-            NSLog(@"\n");
-        }
-
-    
-    cv::Mat mul = intrinsicMatrix * rotationTranslationMatrix;
-    return mul;
+    return join;
 }
 
-//
-//-(Eigen::Matrix<double, 3, 4>) calcProj:(cv::Mat) fundamental {
-//     
-//    return NULL;
-//}
+Eigen::Vector4d convertCartesian(Eigen::Vector4d points) {
+    // Gets rid of the w for a set of points (from homogeneous to cartesian)
+    Eigen::Vector4d p;
+    Eigen::Vector4d ps;
+    p.x() = points.x() / points.w();
+    p.y() = points.y() / points.w();
+    p.z() = points.z() / points.w();
+    p.w() = 1.0;
+    return p;
+}
 
+void debugStuff(Eigen::Vector3d a, Eigen::Vector3d b, Eigen::Matrix<double, 3, 3> f) {
+    Eigen::Matrix<double, 1, 3> c;
+    for (int i = 0; i < b.rows(); i++) {
+        for (int j = 0; j < b.cols(); j++) {
+            c(j, i) = b(i, j);
+        }
+    }
+    
+    Eigen::Matrix<double, 1, 3> d = c * f;
+    double i = d * a;
+    NSLog(@"%f", i);
+}
+
+void checkStuff (Eigen::Matrix<double, 3, 4> p1, Eigen::Matrix<double, 3, 4> p2, Eigen::Matrix<double, 3, 3> f) {
+    Eigen::Matrix<double, 4, 3> c, d;
+    Eigen::Matrix<double, 4, 4> e;
+    
+    for (int i = 0; i < p2.rows(); i++) {
+        for (int j = 0; j < p2.cols(); j++) {
+            c(j, i) = p2(i, j);
+        }
+    }
+    
+    d = c * f;
+    e = d * p1;
+    
+    for (int i = 0; i < e.cols(); i++) {
+        for (int j = 0; j < e.rows(); j++) {
+            NSLog(@"%f", e(i, j));
+        }
+        NSLog(@"\n");
+    }
+}
+
+-(Eigen::Matrix<double, 3, 4>) calcProj:(Eigen::Matrix3d) fundamental {
+    Eigen::JacobiSVD<Eigen::Matrix3d> svd(fundamental, Eigen::ComputeFullU |
+                                          Eigen::ComputeFullV);
+    Eigen::Matrix3d U = svd.matrixU();
+    Eigen::Matrix3d skew, mult;
+    Eigen::Matrix<double, 3, 4> join, done;
+    
+    Eigen::Vector3d v = U.col(2);
+    
+    skew = [self skewSymmetric:v];
+    mult = skew * fundamental;
+    join = joinMatrixVector(mult, v);
+    
+    //    NSLog(@"Join: %i  %i", join.rows(), join.cols());
+    //
+    //    for (int i = 0; i < join.rows(); i++) {
+    //        for (int j = 0; j < join.cols(); j++) {
+    //            NSLog(@"%f", join(i, j));
+    //        }
+    //        NSLog(@"\n");
+    //    }
+    
+    return join;
+}
+
+-(Eigen::Matrix3d) skewSymmetric:(Eigen::Vector3d) v {
+    Eigen::Matrix3d skew;
+    
+    //    NSLog(@"%i  %i   %d", skew.cols(), skew.rows(), skew.size());
+    skew(0, 0) = 0.0;
+    skew(0, 1) = -v(2);
+    skew(0, 2) = v(1);
+    skew(1, 0) = v(2);
+    skew(1, 2) = -v(0);
+    skew(2, 0) = -v(1);
+    skew(2, 1) = v(0);
+    
+    return skew;
+}
 
 - (cv::Mat) cvMatFromUIImage:(UIImage *)i
 {
@@ -305,115 +377,6 @@ cv::Mat getProjectionMatrix(cv::Mat intrinsicMatrix, Matrix3d rotationMatrix, Ve
     return finalImage;
 }
 
-bool testPointInFrontOfCameras(const Eigen::Matrix3d R, const
-                               Eigen::Vector3d& t, const Eigen::Vector3d& p1, const Eigen::Vector3d& p2) {
-    std::vector<Eigen::Matrix<double, 3, 4>> P(2);
-    std::vector<Eigen::Vector3d> p(2);
-    
-    P[0].block<3, 3>(0, 0) = Eigen::Matrix3d::Identity();
-    P[0].col(3) = Eigen::Vector3d::Zero();
-    P[1].block(0,0,3,3) = R;
-    P[1].col(3) = t;
-    
-    p[0] = p1;
-    p[1] = p2;
-    
-    Eigen::Vector4d p3d_h = triangulate(P, p);
-    
-    if (p3d_h(2) / p3d_h(3) < 0) return false;
-    
-    Eigen::Vector3d p2d_h = P[1]*p3d_h;
-    
-    if (p2d_h(2) < 0) return false;
-    return true;
-}
-
-int countPointsInFrontOfCameras(const Eigen::Matrix3d R, const
-                                Eigen::Vector3d& t, const cv::Mat& points1, const cv::Mat& points2) {
-    int count = 0;
-    Eigen::Vector3d test1, test2;
-    for (size_t i = 0; i < points1.rows; ++i) {
-        test1 << points1.at<double>(i,0), points1.at<double>(i,1), 1;
-        test2 << points2.at<double>(i,0), points2.at<double>(i,1), 1;
-        
-        if (testPointInFrontOfCameras(R, t, test1, test2)) ++count;
-    }
-    return count;
-}
-
-void decomposeEssentialMatrix(const cv::Mat& F, const cv::Mat&
-                              points1, const cv::Mat& points2, Eigen::Matrix3d* R, Eigen::Vector3d* t) {
-    Eigen::Matrix3d E;
-    
-    for (size_t r = 0; r < 3; ++r) {
-        for (size_t c = 0; c < 3; ++c) {
-            E(r,c) = F.at<double>(r,c);
-        }
-    }
-    
-    Eigen::JacobiSVD<Eigen::Matrix3d> svd(E, Eigen::ComputeFullU |
-                                          Eigen::ComputeFullV);
-    
-    Eigen::Vector3d svals = svd.singularValues();
-    
-    Eigen::Matrix3d U = svd.matrixU();
-    Eigen::Matrix3d V = svd.matrixV();
-    
-    //Phil - Fix a negative determinant
-    Eigen::Matrix3d tempR = U*V.transpose();
-    
-    if(tempR.determinant() < 0.0) {
-        NSLog(@"decomposeEssentialMatrix: Rotation matrix has negative determinant! Fixing it");
-        V.col(2) *= -1; //Negate the last column
-        //tempR = svd.matrixU()*V.transpose();
-    }
-    
-    Eigen::Matrix3d W(Eigen::Matrix3d::Zero());
-    W(0,1) = -1;
-    W(1,0) = 1;
-    W(2,2) = 1;
-    
-    Eigen::Vector3d u3 = U.col(2);
-    
-    Eigen::Matrix3d R1 = U*W*V.transpose();
-    Eigen::Matrix3d R2 = U*W.transpose()*V.transpose();
-    
-    int maxCount = 0;
-    int thisCount = countPointsInFrontOfCameras(R1, u3, points1, points2);
-    
-    if (thisCount > maxCount) {
-        NSLog(@"Essential Matrix Decomposition: R1, +u3");
-        *R = R1;
-        *t = u3;
-        maxCount = thisCount;
-    }
-    thisCount = countPointsInFrontOfCameras(R1, -u3, points1, points2);
-    if (thisCount > maxCount) {
-        NSLog(@"Essential Matrix Decomposition: R1, -u3");
-        *R = R1;
-        *t = -u3;
-        maxCount = thisCount;
-    }
-    thisCount = countPointsInFrontOfCameras(R2, u3, points1, points2);
-    if (thisCount > maxCount) {
-        NSLog(@"Essential Matrix Decomposition: R2, +u3");
-        *R = R2;
-        *t = u3;
-        maxCount = thisCount;
-    }
-    thisCount = countPointsInFrontOfCameras(R2, -u3, points1, points2);
-    if (thisCount > maxCount) {
-        NSLog(@"Essential Matrix Decomposition: R2, -u3");
-        *R = R2;
-        *t = -u3;
-        maxCount = thisCount;
-    }
-    
-    int numInFront = maxCount;
-    int numBehind = points1.rows - numInFront;
-    
-    NSLog(@"Found %i points in front of cameras, but %i behind", numInFront, numBehind);
-}
 
 - (void)didReceiveMemoryWarning
 {
